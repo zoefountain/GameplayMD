c4fd1070832e1e284db01f85e8dbd70f60fde751
Zoe Fountain <c00289122@setu.ie>
2024-10-02 12:14:02 +0100

    changes to lab.h

--- Project2/Project2/lab3.h
+++ Project2/Project2/lab3.h
@@ -6,7 +6,15 @@
 
 void FindAllVariables(char variables[1000], char code[1000])
 {
+	int isAfunction = 0;
 
+	if (variables - 1 == 'i' && variables - 2 == 'n' && variables - 3 == 't' && variables == '')
+	{
+		if (variables - 4 == 'c' && variables - 3 == 'h' && variables - 2 == 'a' && variables - 1 == 'r' && variables == '')
+		{
+
+		}
+	}
 }
 void FindAllVariablesInScope(char variables[1000], char code[1000], int lineNumber)
 {
